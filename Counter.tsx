3a05678ab21e66cc92195cf2acb5744c97453274
Mansi <73159092+Mansi157@users.noreply.github.com>
2026-03-23 17:41:58 +0530

    Update Counter.tsx

--- Counter.tsx
+++ Counter.tsx
@@ -1,23 +1,38 @@
 'use client'
-
+import {ChangeEvent, ChangeEventHandler, useRef, useState} from 'react'
 type CounterProps = {
     count: number;
 }
 
 export default function Counter(props: CounterProps) {
-    let count=props.count;
+    ///let count=props.count;
+    const [count,setCount]=useState(props.count);
+    const inputRef=useRef<HTMLInputElement>(null);
     function inc(){
         console.log("incrementing counter...")
-        count++;
+        setCount(count+1);
     }
     
+    function handleChange(evt:ChangeEvent<HTMLInputElement>){
+     setCount(evt.target.valueAsNumber);
+    }
     return (
     <div>
         <h4>Count:{props.count}</h4>
         <div>
             <button onClick={inc}>++</button> &nbsp;
-            <button>--</button>
+            <button onClick={()=> setCount(count-1)}>--</button>
+        </div>
+<br />
+        <div>
+            <input type="number" value={count} onChange={handleChange}/>
+        </div>
+
+        <div>
+          <input ref={inputRef} type="number" placeholder="Enter the number"/> &nbsp;
+          <button onClick={()=>(inputRef.current?.valueAsNumber || 0)}>updatecount </button>
+
         </div>
     </div>
-    )
+    );
 }
